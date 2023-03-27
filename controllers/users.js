fd4a08ee08b5efd299fa8d50326ec819e963b6ad
Jean Balangue <jeanbalangue@hotmail.fr>
2023-03-27 13:13:04 +0200

    users specs

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,12 @@
 const User = require('../models/user')
 
 exports.getUsers = (req, res, next) => {
-  const users = [
-    {
-      _id: "oeihfzeoi",
-      name: "xaxa",
-      username: "xaxa",
-      email: "jeanbalangue@hotmail.fr",
-      phone: "1111111111",
-      address: "1111111111",
-      company: "1111111111",
-      website: "1111111111",
-      userId: "xaxa",
-    }
-  ]
-  res.status(200).json(users)
+  User.find().then((users) => {
+      res.status(200).json(users)
+  })
+  .catch((error => {
+    res.status(401).json({error})
+  }))
 }
 
 exports.postUser = (req, res, next) => {
@@ -27,14 +19,3 @@ exports.postUser = (req, res, next) => {
       .catch(error => res.status(400).json({ error }))
 }
 
-// {
-//   "_id": "oeihfzeoi",
-//   "name": "xaxa",
-//   "username": "xaxa",
-//   "email": "jeanbalangue@hotmail.fr",
-//   "phone": "1111111111",
-//   "address": "1111111111",
-//   "company": "1111111111",
-//   "website": "1111111111",
-//   "userId": "xaxa"
-// }
--- spec/controllers/users.spec.js
+++ spec/controllers/users.spec.js
@@ -1,5 +1,69 @@
-describe('ca marche', ()=>{
-  it('should show tru on users', () => {
-    expect(true).toEqual(true)
+const userCtrl = require("../../controllers/users")
+const User = require('../../models/user')
+const http = require('http');
+
+const users = [{
+  // _id: "oeihfzeoi",
+  name: "xaxa",
+  username: "xaxa",
+  email: "jeanbalangue@hotmail.fr",
+  phone: "1111111111",
+  address: "1111111111",
+  company: "1111111111",
+  website: "1111111111",
+  userId: "xaxa",
+}]
+
+const getUsersOptions = {
+  hostname: 'localhost',
+  port: 3000,
+  path: '/users',
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}
+
+const postUsersOptions = {
+  hostname: 'localhost',
+  port: 3000,
+  path: '/users',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body : users[0]
+};
+
+describe('getUsers() should work :)', () => {
+  it('should return an array of users and status code', (done) => {
+    let req = http.request(getUsersOptions, (res) => {
+      res.on('data', (elements) => {
+        data = JSON.parse(elements)
+        if (Object.keys(data).length > 0){
+          expect(new User({...data[0]})).toBeTruthy()
+          expect(new User({...data[0]})).toBeInstanceOf(User)
+          expect(res.statusCode).toBe(200)
+        } else {
+          expect(res.statusCode).toBe(401)
+        }
+      })
+      done() // permet d'attendre que la promise soit resolue avant d'expecter
+    })
+    req.end() // permet de lancer la requete ...
+  })
+})
+
+
+describe('posttUsers() should work :)', () => {
+  it('should return 200 status code', (done) => {
+    let req = http.request(postUsersOptions, (res) => {
+      res.on('data', (elements) => {
+        data = JSON.parse(elements)
+          expect(res.statusCode).toBe(200)
+        })
+      })
+      done() 
+      req.end()
   })
 })
